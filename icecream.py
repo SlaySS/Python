46e9327cfe46c6aa28203922cde72d01950bbbec
Svyatoslav Samoilov <slaysam@yandex.ru>
2020-05-22 14:09:28 +0500

    plus icecream

--- icecream.py
+++ icecream.py
@@ -3,11 +3,17 @@ class IceCream:
         self.flavor = flavor
         self.sprinkles = sprinkles
 
-def sweetest_icecream(lst):
-    flavor_values = {
-        "Plain": 0,
-        "Vanilla": 5,
-        "ChokolateChip": 5,
-        "Strawberry": 10,
-        "Chokolate": 10
-    }
+    def sweetest_icecream(lst):
+        flavor_values = {
+            "Plain": 0,
+            "Vanilla": 5,
+            "ChokolateChip": 5,
+            "Strawberry": 10,
+            "Chokolate": 10
+        }
+        return max([icecream.sprinkles + flavor_values[icecream.flavor] for icecream in lst])
+
+
+ice1 = IceCream("ChokolateChip" , 10)
+ice2= IceCream("Vanilla" , 5)
+print(IceCream.sweetest_icecream([ice1, ice2]))
