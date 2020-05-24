75d3f25772eebcd057a5eb4b535a01ea710cd0ac
Slaysam <salysam66@gmail.com>
2020-05-24 20:06:24 +0500

    ugadai slovo

---
+++ ugadai_slovo.py
@@ -0,0 +1,27 @@
+import random
+
+file_with_words = "original.txt"
+
+file = open(file_with_words, mode="r", encoding="utf-8")
+
+random_lines = random.choice(file.readlines())
+word = random_lines.strip('\n')
+print(word)
+
+
+secret_word = []
+for i in word:
+    secret_word.append("_")
+
+print(secret_word)
+
+while True:
+    letter = input("Веедите букву :")
+    if letter in word:
+        print("Такая буква есть !")
+        secret_word.insert(word.index(letter), letter)
+        secret_word.pop()
+        print(secret_word)
+    else:
+        print("Такой буквы нет")
+
