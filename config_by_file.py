5d66291b55797eda9c4c96fd7a8a7fd54b787fa8
Svyatoslav Samoilov <slaysam@yandex.ru>
2020-05-25 14:03:38 +0500

    config parser

---
+++ config_by_file.py
@@ -0,0 +1,19 @@
+import configparser  # импортируем библиотеку
+
+config = configparser.ConfigParser()  # создаём объекта парсера
+config.read("config.ini")  # читаем конфиг
+
+file_to_work = (config["Resources"]["file"])
+
+f = open('%s' % file_to_work, mode="r")
+
+ip_address = []
+
+for line in f:
+    # print(line, end='') #end='', не переводить строку
+    ip_address.append(line.strip())
+# print(work_dir)
+
+print(ip_address)
+
+f.close()
---
+++ work_with_file_2.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print("Необходимо передать рабочую дирекрорию в качестве аргумента командной строки")
+    sys.exit()
+
+
+file_name = sys.argv[1]
+
+f = open('%s' %file_name, mode = "r") # использоват переменную в качестве имени файла
+ip_address = []
+
+for line in f:
+    # print(line, end='') #end='', не переводить строку
+    ip_address.append(line.strip())
+# print(work_dir)
+
+print(ip_address)
+
+f.close()
