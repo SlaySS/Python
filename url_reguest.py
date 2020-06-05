e37629507f3135d1208cd7f972d5f3e3995e5db7
Svyatoslav Samoilov <slaysam@yandex.ru>
2020-06-05 10:18:28 +0500

    obnova

--- url_reguest.py
+++ url_reguest.py
@@ -1,14 +1,9 @@
 import sys
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-# url = sys.argv[1]
 
 # r = requests.get('https://api.github.com/events')
-r = requests.get('https://insite.emb.bank/', verify=False)
+r = requests.get('https://repo.spring.io/', verify=True)
 # otvet = requests.get("http://insite/")
 # print(otvet)
-print(r.status_code)
+print(r.text)
--- work_with_file2.py
+++ work_with_file2.py
@@ -1,6 +1,10 @@
-file = 'test.txt'
+import sys
+
+file = sys.argv[1]
 
 with open(file, mode='r', encoding='utf-8') as open_file:
-    line = open_file.read()
-    print(line)
-  
+    line = open_file.readlines()
+    for l in line:
+        line = "---" + l
+        if line.find("aaa") != - 1:
+            print(line[::-1], end='')
